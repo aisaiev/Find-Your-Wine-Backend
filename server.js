16f0abba840e3465076713fc1e5689e1fa42087c
Anton <antonio.de.tores@gmail.com>
2021-01-07 17:47:19 +0200

    port changed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const vivinoService = require('./vivino.service');
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
